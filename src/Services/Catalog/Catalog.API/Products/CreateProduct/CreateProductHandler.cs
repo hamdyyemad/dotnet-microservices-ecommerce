c36e69fdf665692f44439f0e359876f413046360
Hamdy Emad <hamdyyemad@aol.com>
2025-09-26 02:09:45 +0300

    mplement CreateProduct feature using MediatR, Carter, and Mapster
    - Added CreateProductCommand and handler with MediatR for CQRS pattern
    - Created CreateProductEndpoint with Carter for minimal API routing
    - Utilized Mapster for mapping between request DTO and command
    - Structured code for clean separation and maintainability

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -10,13 +11,25 @@ namespace Catalog.API.Products.CreateProduct
 
     internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Create Product Entity from Command Object
-            // Save to DB
-            // Return CreateProductResult
+            // Step1. Create Product Entity from Command Object
+            Product product = new Product 
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
 
-            return new CreateProductResult(Guid.NewGuid());
+            // TODO
+            // Step2. Save to DB
+            // It will return as back and id and we will pass that id to the response
+            product.Id = Guid.NewGuid();
+
+            // Step3. Return CreateProductResult
+            return new CreateProductResult(product.Id);
         }
     }
 }

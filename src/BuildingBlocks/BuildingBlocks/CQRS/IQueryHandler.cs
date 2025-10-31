3ff2eb605d4ef1e0a530421dabc15ac631f62c7e
Hamdy Emad <hamdy.e@automatedpros.io>
2025-10-31 03:16:13 +0200

    feat: adding GetProducts feature using MediatR and adding port for .env in the catalog.api service

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -1,7 +1,8 @@
 ﻿using MediatR;
+
 namespace BuildingBlocks.CQRS
 {
-    internal interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
         where TQuery : IQuery<TResponse>
         where TResponse : notnull
     {
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsQueryHandler(IDocumentStore store)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            using var session = store.LightweightSession();
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+}
